3f2eaf9b6236963697139214d861f81c6fb6c547
Guillaume <guillaume.toussaint1@etu.univ-lorraine.fr>
2018-09-26 16:04:06 +0200

    Correction package

--- Projet-Taquin/src/projettaquin/FXMLDocumentController.java
+++ Projet-Taquin/src/projettaquin/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projet.taquin;
+package projettaquin;
 
 import java.net.URL;
 import java.util.ResourceBundle;
