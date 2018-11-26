0acb556c346820fd2ae0f3d346d857b45be8e04f
Guillaume <guillaume.toussaint1@etu.univ-lorraine.fr>
2018-11-26 17:22:43 +0100

    Ajout structure strategy pour algo IA

---
+++ Projet-Taquin/src/utils/AStar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author guillaume
+ */
+public class AStar implements AlgoIA{
+
+    @Override
+    public Stack aide() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Projet-Taquin/src/utils/Aide.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author guillaume
+ */
+public class Aide {
+    private AlgoIA algo;
+    
+    
+    public Aide(String typeAlgo){
+        if(typeAlgo.equals("A*")){
+            
+        }else if(typeAlgo.equals("IDA*")){
+            
+        }
+    }
+}
