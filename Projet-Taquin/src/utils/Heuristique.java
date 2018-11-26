c11de8b9b111b31516455cd6a10e2d3a1c13436e
Guillaume <guillaume.toussaint1@etu.univ-lorraine.fr>
2018-11-26 17:25:45 +0100

    Ajout squelette heuristique

---
+++ Projet-Taquin/src/utils/Heuristique.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import taquinconsole.Plateau;
+
+/**
+ *
+ * @author guillaume
+ */
+public class Heuristique {
+    private Plateau grille;
+    
+    public Heuristique(Plateau g){
+        this.grille = g;
+    }
+    
+    public int cout(){
+        int res = 0;
+        
+        
+        
+        return res;
+    }
+}
