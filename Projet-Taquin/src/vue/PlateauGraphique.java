3d9a93c2dbf8b9d1ecf94100d0f79e607b8fb6e1
lilisanta <lilisanta@github.com>
2018-11-30 13:22:38 +0100

    Problème de positionnement des labaels reglé

--- Projet-Taquin/src/vue/PlateauGraphique.java
+++ Projet-Taquin/src/vue/PlateauGraphique.java
@@ -7,8 +7,16 @@ package vue;
 
 
 import java.io.IOException;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.ImagePattern;
 import taquinconsole.Case;
 import taquinconsole.CaseNumerotee;
 import taquinconsole.GenerateurPlateau;
@@ -29,7 +37,7 @@ public class PlateauGraphique extends GridPane{
         this.setMinHeight(420);
         try {
             plateau=GenerateurPlateau.chargerPlateau("taquin1");
-            ImageUtils.split("crash_test_taquin.jpg",4);
+            ImageUtils.split("crash_test_taquin.jpg",3);
             Case [][] cases = plateau.getCases();
             int compte = 1; 
             for (int i =0 ; i <cases.length ; i++) {
@@ -37,8 +45,14 @@ public class PlateauGraphique extends GridPane{
                     Case courante = cases [i][j];
                     if(courante instanceof CaseNumerotee) {
                         Label l = new Label(""+((CaseNumerotee) courante).getNum()); 
-                       //l.setstyle("-fxbackground-image: url(∖"media/images/tmp/"+compte+".png∖");");
-                        this.add(l,i,j);
+                      // l.setStyle("-fx-background-image: url('media/images/tmp/"+compte+".png∖');");
+                      l.setMinWidth(420/3);
+                      l.setMinHeight(420/3);
+                      l.setAlignment(Pos.CENTER);
+                      String source = "file:///"+System.getProperty("user.dir")+"∖∖media∖∖images∖∖tmp∖∖"+compte+".png";
+                      source=source.replace("∖∖", "/");
+                      l.setBackground(new Background(new BackgroundFill(new ImagePattern(new Image(source)),CornerRadii.EMPTY,Insets.EMPTY)) );  
+                      this.add(l,i,j);
                         compte++;
                     }
                 }
