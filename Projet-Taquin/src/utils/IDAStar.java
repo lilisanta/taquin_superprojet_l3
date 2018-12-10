226681bea26f10f9dc323fcf97461d566fcd7589
Guillaume <guillaume.toussaint1@etu.univ-lorraine.fr>
2018-12-10 16:32:28 +0100

    Modification IA

--- Projet-Taquin/src/utils/IDAStar.java
+++ Projet-Taquin/src/utils/IDAStar.java
@@ -5,7 +5,11 @@
  */
 package utils;
 
+import static java.lang.Math.abs;
 import java.util.Stack;
+import taquinconsole.Case;
+import taquinconsole.CaseNumerotee;
+import taquinconsole.Plateau;
 
 /**
  *
@@ -16,6 +20,25 @@ public class IDAStar implements AlgoIA{
     @Override
     public Stack aide() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int cout(Plateau grille){
+        int res = 0;
+        Case[][] cases = grille.getCases();
+        for(int i = 0; i < cases.length; i++){
+            for(int j =0; j < cases[i].length; i++){
+                Case courante = cases[i][j];
+                if(courante instanceof CaseNumerotee){
+                    int diff = ((CaseNumerotee) courante).getNum() - (3*i+j);
+                    if(diff!=0){
+                        res++;
+                    }
+                }
+            }
+        }
+        
+        
+        return res;
     }
     
 }
